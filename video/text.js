6c7df93cc23427851e9df286f34fd574fbeeea86
Richard Šefr <richard.sefr@gmail.com>
2019-01-27 21:33:48 +0100

    Enhance introduction text

--- video/text.js
+++ video/text.js
@@ -2,13 +2,17 @@ var cameraText;
 var terminalText;
 
 const introductionText = `
+  <h1>Reckless</h1>
+
+  <p>Location: Orbit of Omicron 6<br />
+  Date: November 30, 2279</p>
   <p>You and your team noticed that a space station did not respond to your requests about the status near your destination at Omicron 6. As is the law, every spaceship has to investigate a potential power or oxygen defect when passing any vessel.</p>
-  <p>While you were trying to reach anyone else assuming that the personnel at the station might require help, the rest of your crew approached and docked the station in one of your modules.</p>
+  <p>While you were trying to reach anyone else assuming that the personnel at the station might require further help, the rest of your crew approached and docked the station in one of your modules.</p>
   <p>Shortly after your crew entered the station, all communication went dark. After a while you finally managed to gain access to basic systems of the station. You now see that the station is severely damaged from the inside and is <u>infested by multiple dangerous organisms</u> in every room.</p>
-  <p>As the crew's module was damaged due to the fight with several of these organisms, you now have to lead your crew to one of the station's escape module on the other side. However, the <u>doors between each room have been closed</u> to keep the integrity of the station intact and have to be <u>unlocked by finding the right signal</u> for each door.</p>
+  <p>As the crew's module was damaged due to the fight with several of these organisms, you now have to lead your crew to one of the station's escape module on the other side. However, the <u>doors between each room have been automatically closed</u> to keep the integrity of the station intact and have to be <u>unlocked by finding the right signal</u> for each door.</p>
   <p>Lead your crew to safety before they are overrun.</p>
   </br>
-  <p>Click to proceed &gt;</p>
+  <p>&gt; Click to proceed</p>
 `;
 
 const helpText = `
